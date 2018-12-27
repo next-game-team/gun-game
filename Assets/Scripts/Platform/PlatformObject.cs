24e0951d26480b45d9ef89c64b0849271ff0eb33
Nikita Chernyakov <nikchernyakov@yandex.ru>
2018-12-27 23:03:26 +0300

    Create PlatformObject

---
+++ Assets/Scripts/Platform/PlatformObject.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformObject : MonoBehaviour
+{
+
+    [SerializeField, ReadOnly]
+    private Platform _currentPlatform;
+
+    [SerializeField] 
+    private Transform _objectBottomPosition;
+
+    public Transform ObjectBottomPosition => _objectBottomPosition;
+
+    public Platform CurrentPlatform => _currentPlatform;
+
+    private void Awake()
+    {
+        _objectBottomPosition = GameObjectUtils.GetChildrenWithTag(gameObject, TagEnum.PlatformObjectBottom).transform;
+    }
+
+}
