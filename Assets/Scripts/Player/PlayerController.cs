2a43526770bdc6c2daa78f63451b686f0eeb1b66
Nikita Chernyakov <nikchernyakov@yandex.ru>
2018-12-28 23:16:28 +0300

    Create PlayerController

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MoveController))]
+public class PlayerController : MonoBehaviour
+{
+    private MoveController _moveController;
+    
+    // Start is called before the first frame update
+    void Awake()
+    {
+        _moveController = GetComponent<MoveController>();
+        _moveController.MoveEvent.AddListener(OnMoveEvent);
+    }
+
+    private void OnMoveEvent(DirectionEnum directionEnum)
+    {
+        
+    }
+}
