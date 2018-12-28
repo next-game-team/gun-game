31b31e6d59ccea2c410e534af5b263d3122d760c
Nikita Chernyakov <nikchernyakov@yandex.ru>
2018-12-28 23:16:18 +0300

    Create KeyController

---
+++ Assets/Scripts/Player/Move/KeyController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyController : MoveController
+{
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            MoveEvent.Invoke(DirectionEnum.UP);
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            MoveEvent.Invoke(DirectionEnum.DOWN);
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            MoveEvent.Invoke(DirectionEnum.LEFT);
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            MoveEvent.Invoke(DirectionEnum.RIGHT);
+        }
+    }
+}
