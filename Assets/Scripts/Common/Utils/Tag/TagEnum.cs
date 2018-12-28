aac27e9f80c6f919ce5a57d0d031c7c88c43558d
Nikita Chernyakov <nikchernyakov@yandex.ru>
2018-12-28 22:41:13 +0300

    Update PlatformMap search behavior
    Now map is a table

--- Assets/Scripts/Common/Utils/Tag/TagEnum.cs
+++ Assets/Scripts/Common/Utils/Tag/TagEnum.cs
@@ -6,5 +6,6 @@
     // Platform
     Platform,
     PlatformTopCenter,
-    PlatformObjectBottom
+    PlatformObjectBottom,
+    FirstPlatform
 }
--- Assets/Scripts/Platform/PlatformMap.cs
+++ Assets/Scripts/Platform/PlatformMap.cs
@@ -6,12 +6,45 @@ public class PlatformMap : MonoBehaviour
 {
 
     [SerializeField] 
-    private List<Platform> _platforms;
+    private List<List<Platform>> _platforms;
 
-    public List<Platform> Platforms => _platforms;
+    [SerializeField, ReadOnly] 
+    private readonly int _rowCount;
+    [SerializeField, ReadOnly] 
+    private readonly int _columnCount;
+
+    public List<List<Platform>> Platforms => _platforms;
 
     private void OnValidate()
     {
-        _platforms = GetComponentsInChildren<Platform>().ToList();
+        var firstPlatformInCurrentRow = GameObjectUtils.GetChildrenWithTag(gameObject, TagEnum.FirstPlatform)
+            .GetComponent<Platform>();
+
+        if (firstPlatformInCurrentRow == null)
+        {
+            Debug.LogError("There is no first platform in map");
+            return;
+        }
+
+        _platforms = new List<List<Platform>>();
+        while (firstPlatformInCurrentRow != null)
+        {
+           _platforms.Add(GetPlatformsInCurrentRow(firstPlatformInCurrentRow));
+           firstPlatformInCurrentRow = firstPlatformInCurrentRow.Neighbors.Bottom;
+        }
+    }
+
+    private static List<Platform> GetPlatformsInCurrentRow(Platform firstPlatformInRow)
+    {
+        var row = new List<Platform>();
+        var currentPlatform = firstPlatformInRow;
+        while (currentPlatform != null)
+        {
+            currentPlatform.GetComponent<PlatformNeighborsFinder>().FindNeighbors();
+            row.Add(currentPlatform);
+            currentPlatform = currentPlatform.Neighbors.Right;
+        }
+
+        return row;
     }
 }
