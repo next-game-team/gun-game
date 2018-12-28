7335c651742ec4a5735e6e5215117955e11cf409
Nikita Chernyakov <nikchernyakov@yandex.ru>
2018-12-28 23:16:10 +0300

    Create MoveController

---
+++ Assets/Scripts/Player/Move/MoveController.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class MoveEvent : UnityEvent<DirectionEnum> {}
+
+public abstract class MoveController : MonoBehaviour
+{
+    public MoveEvent MoveEvent { get; } = new MoveEvent(); 
+}
