d2f40358fa6e6e7fa1031531593569795bab9c6f
Nikita Chernyakov <nikchernyakov@yandex.ru>
2018-12-28 22:55:43 +0300

    Create BetweenPlatformMover

---
+++ Assets/Scripts/Platform/BetweenPlatformMover.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class BetweenPlatformMover
+{
+    public static void MoveTo(PlatformObject platformObject, DirectionEnum directionEnum)
+    {
+        platformObject.CurrentPlatform.EmptyPlatform();
+        var newPlatform = GetNeighborByDirection(platformObject.CurrentPlatform, directionEnum);
+        if (newPlatform == null)
+        {
+            Debug.LogError("Move to wrong direction");
+        }
+            
+        newPlatform.PlatformObject = platformObject;
+    }
+
+    public static Platform GetNeighborByDirection(Platform platform, DirectionEnum directionEnum)
+    {
+        switch (directionEnum)
+        {
+            case DirectionEnum.LEFT:
+                return platform.Neighbors.Left;
+            case DirectionEnum.RIGHT:
+                return platform.Neighbors.Right;
+            case DirectionEnum.UP:
+                return platform.Neighbors.Top;
+            case DirectionEnum.DOWN:
+                return platform.Neighbors.Bottom;
+            default:
+                return null;
+        }
+    }
+}
