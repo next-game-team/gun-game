ef9d78908994227699d00967dee1d83523a2cbb2
PsyGameStud <psygamestud@gmail.com>
2018-12-29 17:28:19 +0300

    Update
    Added SwipeController

---
+++ Assets/Scripts/Player/Move/SwipeController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class SwipeController : MoveController, IBeginDragHandler, IDragHandler
+{
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        if(eventData.delta.x > 0)
+        {
+            MoveEvent.Invoke(DirectionEnum.RIGHT);
+        } else if(eventData.delta.x < 0)
+        {
+            MoveEvent.Invoke(DirectionEnum.LEFT);
+        } else if (eventData.delta.y > 0)
+        {
+            MoveEvent.Invoke(DirectionEnum.UP);
+        } else if (eventData.delta.y < 0)
+        {
+            MoveEvent.Invoke(DirectionEnum.DOWN);
+        }
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+    }
+
+}
