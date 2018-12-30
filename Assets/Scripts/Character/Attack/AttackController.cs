7180e5561e14368badb25efe7021c29533c3d175
Nikita Chernyakov <nikchernyakov@yandex.ru>
2018-12-30 09:50:44 +0300

    Create AttackController

---
+++ Assets/Scripts/Character/Attack/AttackController.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class AttackEvent : UnityEvent {}
+
+public abstract class AttackController : MonoBehaviour
+{
+    public AttackEvent AttackEvent { get; } = new AttackEvent(); 
+}
