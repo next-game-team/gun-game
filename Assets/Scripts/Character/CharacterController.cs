47c31605c4aeb2c5e4cca388ebd8c2367634c518
Nikita Chernyakov <nikchernyakov@yandex.ru>
2018-12-30 09:51:36 +0300

    Update CharacterController
    - Small code style fixes
    - Add handling attack event

---
+++ Assets/Scripts/Character/CharacterController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlatformObject))]
+[RequireComponent(typeof(MoveController))]
+[RequireComponent(typeof(AttackController))]
+public class CharacterController : MonoBehaviour
+{
+    private PlatformObject _platformObject;
+    private MoveController _moveController;
+    private AttackController _attackController;
+    
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        _platformObject = GetComponent<PlatformObject>();
+        
+        _moveController = GetComponent<MoveController>();
+        _moveController.MoveEvent.AddListener(OnMoveEvent);
+        
+        _attackController = GetComponent<AttackController>();
+        _attackController.AttackEvent.AddListener(OnAttackEvent);
+    }
+
+    private void OnMoveEvent(DirectionEnum directionEnum)
+    {
+        BetweenPlatformMover.MoveTo(_platformObject, directionEnum);
+    }
+
+    private void OnAttackEvent()
+    {
+        
+    }
+}
