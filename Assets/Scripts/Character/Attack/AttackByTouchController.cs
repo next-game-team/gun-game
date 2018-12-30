1b1cd80ba360e7db6f27984fac7dfb28ddc913c6
PsyGameStud <psygamestud@gmail.com>
2018-12-30 14:35:33 +0300

    Create
    Added a new plugin - cinemachine
    Added script - AttackByTouchController

---
+++ Assets/Scripts/Character/Attack/AttackByTouchController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackByTouchController : AttackController
+{
+    void Update() 
+    {
+        if(Input.touchCount > 0) //to change the logic
+        {
+            AttackEvent.Invoke();
+        }
+    }
+}
