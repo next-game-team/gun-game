801c45253ee0256a69ce537b2b8cc6801b52da7e
Nikita Chernyakov <nikchernyakov@yandex.ru>
2018-12-30 10:49:02 +0300

    Create GunConfig

---
+++ Assets/Scripts/Config/GunConfig.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "GunConfig", menuName = "Create Gun config")]
+public class GunConfig : ScriptableObject
+{
+    public float RotationSpeed;
+    public float CooldownTime;
+    public int DamageCount;
+    public float BulletSpeed;
+}
--- Assets/Scripts/Gun/Gun.cs
+++ Assets/Scripts/Gun/Gun.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Gun : MonoBehaviour
 {
+    [SerializeField] private GunConfig _gunConfig;
+    
     // Start is called before the first frame update
     void Start()
     {
