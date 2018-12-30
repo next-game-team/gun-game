96f9d7eaf720f81c4baa03ae5b2105700dbc8737
Nikita Chernyakov <nikchernyakov@yandex.ru>
2018-12-30 09:27:04 +0300

    Update PlayerController.cs
    Add moving from one platform to next by move event

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -3,19 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(MoveController))]
+[RequireComponent(typeof(PlatformObject))]
 public class PlayerController : MonoBehaviour
 {
     private MoveController _moveController;
+    private PlatformObject _platformObject;
     
     // Start is called before the first frame update
     void Awake()
     {
         _moveController = GetComponent<MoveController>();
         _moveController.MoveEvent.AddListener(OnMoveEvent);
+
+        _platformObject = GetComponent<PlatformObject>();
     }
 
     private void OnMoveEvent(DirectionEnum directionEnum)
     {
-        
+        BetweenPlatformMover.MoveTo(_platformObject, directionEnum);
     }
 }
