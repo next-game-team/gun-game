e83bcb0afe92ae267f8c04000e177bded0dfdcd8
Nikita Chernyakov <nikchernyakov@yandex.ru>
2018-12-31 11:54:39 +0300

    Update PlatformMap.cs
    Add NeighborsFinder to Map

--- Assets/Scripts/Platform/PlatformMap.cs
+++ Assets/Scripts/Platform/PlatformMap.cs
@@ -9,14 +9,23 @@ public class PlatformMap : MonoBehaviour
     private List<List<Platform>> _platforms;
 
     [SerializeField, ReadOnly] 
-    private readonly int _rowCount;
+    private int _rowCount;
     [SerializeField, ReadOnly] 
-    private readonly int _columnCount;
+    private int _columnCount;
 
+    private PlatformNeighborsFinder _neighborsFinder;
+    
     public List<List<Platform>> Platforms => _platforms;
 
     private void OnValidate()
     {
+        _neighborsFinder = GetComponent<PlatformNeighborsFinder>();
+        if (_neighborsFinder == null)
+        {
+            Debug.LogWarning("PlatformNeighborsFinder doesn't set on PlatformMap");
+            return;
+        } 
+        
         var firstPlatformInCurrentRow = GameObjectUtils.GetChildrenWithTag(gameObject, TagEnum.FirstPlatform)
             .GetComponent<Platform>();
 
@@ -32,15 +41,18 @@ public class PlatformMap : MonoBehaviour
            _platforms.Add(GetPlatformsInCurrentRow(firstPlatformInCurrentRow));
            firstPlatformInCurrentRow = firstPlatformInCurrentRow.Neighbors.Bottom;
         }
+
+        _rowCount = _platforms.Count;
+        _columnCount = _platforms[0].Count;
     }
 
-    private static List<Platform> GetPlatformsInCurrentRow(Platform firstPlatformInRow)
+    private List<Platform> GetPlatformsInCurrentRow(Platform firstPlatformInRow)
     {
         var row = new List<Platform>();
         var currentPlatform = firstPlatformInRow;
         while (currentPlatform != null)
         {
-            currentPlatform.GetComponent<PlatformNeighborsFinder>().FindNeighbors();
+            _neighborsFinder.FindNeighbors(currentPlatform);
             row.Add(currentPlatform);
             currentPlatform = currentPlatform.Neighbors.Right;
         }
