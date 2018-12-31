7ea76095e75565c39296dc9a3fbf1105a6fda0c1
Nikita Chernyakov <nikchernyakov@yandex.ru>
2018-12-31 12:13:53 +0300

    Create PlayerStartPositionController

---
+++ Assets/Scripts/Game/PlayerStartPositionController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class PlayerStartPositionController : MonoBehaviour
+{
+    [SerializeField]
+    private Platform _startPlatform;
+
+    [SerializeField] 
+    private PlatformObject _player;
+
+    private void Awake()
+    {
+        if (_startPlatform == null || _player == null)
+        {
+            Debug.LogWarning("PlayerStartPositionController doesn't have objects");
+            return;
+        }
+
+        _startPlatform.PlatformObject = _player;
+        _player.transform.position = _startPlatform.CenterOfTopBound.position
+                                            + _player.VectorFromBottomToCenter;
+    }
+}
