9ec7624e3f8285a626610cf896f1c8e2d84361be
Nikita Chernyakov <nikchernyakov@yandex.ru>
2019-01-01 13:42:17 +0300

    Create BulletPool

--- Assets/Scripts/Common/Patterns/Pool/PoolManager.cs
+++ Assets/Scripts/Common/Patterns/Pool/PoolManager.cs
@@ -2,9 +2,11 @@
 
 public class PoolManager : Singleton<PoolManager> {
     
+    public Pool BulletPool { get; private set; }
+
     private void Awake()
     {
-        // Initialize all your pools
+        BulletPool = InitPool(TagEnum.BulletPool);
     }
 
     private static Pool InitPool(TagEnum tagEnum)
