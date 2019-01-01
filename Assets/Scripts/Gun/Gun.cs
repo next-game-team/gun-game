e83d8d88e9cb0e3bfe4d0ef1017baa4cd29f75ce
Nikita Chernyakov <nikchernyakov@yandex.ru>
2019-01-01 13:45:10 +0300

    Update Gun.cs
    - Add cooldown
    - Add shoot logic

--- Assets/Scripts/Gun/Gun.cs
+++ Assets/Scripts/Gun/Gun.cs
@@ -5,21 +5,40 @@ using UnityEngine;
 public class Gun : MonoBehaviour
 {
     [SerializeField] private GunConfig _gunConfig;
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+
+    [SerializeField] private Transform _shootPoint;
+
+    private bool _isInCooldown;
+    private float _currentCooldownTime;
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (!_isInCooldown) return;
+
+        if (_isInCooldown && _currentCooldownTime > 0)
+        {
+            _currentCooldownTime -= Time.deltaTime;
+        }
+        else
+        {
+            _isInCooldown = false;
+        }
     }
 
     public void Shoot()
     {
+        if (_isInCooldown) return;
+
+        _isInCooldown = true;
+        _currentCooldownTime = _gunConfig.CooldownTime;
         
+        var bullet = PoolManager.Instance.BulletPool.GetObject(_shootPoint.position).GetComponent<Bullet>();
+        bullet.Init(_gunConfig.BulletConfig, GetCurrentVelocityVector(), transform.rotation);
+    }
+
+    private Vector2 GetCurrentVelocityVector()
+    {
+        return _shootPoint.position - transform.position;
     }
 }
