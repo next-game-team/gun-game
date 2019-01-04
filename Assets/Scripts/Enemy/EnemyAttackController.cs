d6ef8b85e0415e6013d2cf79473ad48384af149a
PsyGameStud <psygamestud@gmail.com>
2019-01-04 15:08:52 +0300

    Create EnemyAttackController

---
+++ Assets/Scripts/Enemy/EnemyAttackController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttackController : AttackController
+{
+    [SerializeField] private Transform _shootPoint; //change
+    [SerializeField] private float _raycastLength;
+    [SerializeField] private LayerMask _player;
+
+    void Update()
+    {   
+        if(FindPlayer())
+        {
+            AttackEvent.Invoke();
+        }
+    }
+
+    private bool FindPlayer()
+    {
+        if(Physics2D.Raycast(_shootPoint.position, Vector3.up, _raycastLength, _player))
+        {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
