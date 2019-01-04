add0918b24e9b4599bb5845bbe0655ccb62a8818
Nikita Chernyakov <nikchernyakov@yandex.ru>
2019-01-04 15:14:43 +0300

    Update EnemyGenerator.cs
    Add generating logic

--- Assets/Scripts/Enemy/Generation/EnemyGenerator.cs
+++ Assets/Scripts/Enemy/Generation/EnemyGenerator.cs
@@ -1,11 +1,15 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class EnemyGenerator : Singleton<EnemyGenerator>
 {
     public void GenerateRandomEnemyOnRandomPosition()
     {
+        var freePlatform = RandomUtils.GetRandomObjectFromList(PlatformMap.Instance.FreePlatforms);
+        if (freePlatform == null)
+        {
+            Debug.LogWarning("There is no free platform");
+        } 
         
+        freePlatform.SetPlatformObject(PoolManager.Instance.EnemyPool.GetObject().GetComponent<PlatformObject>());
     }
 }
