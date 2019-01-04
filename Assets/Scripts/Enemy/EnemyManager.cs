2c043ae57787a67197339cd7f79e607cdb267e67
Nikita Chernyakov <nikchernyakov@yandex.ru>
2019-01-04 12:16:56 +0300

    Create EnemyManager

---
+++ Assets/Scripts/Enemy/EnemyManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : Singleton<EnemyManager>
+{
+    [SerializeField, ReadOnly]
+    private GameObject _player;
+
+    public GameObject Player => _player;
+    
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        _player = GameObject.FindGameObjectWithTag(TagUtils.GetTagNameByEnum(TagEnum.Player));
+    }
+}
