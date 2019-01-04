4fb6899394a9587ca767be2c4ef0551f0a5c7766
Nikita Chernyakov <nikchernyakov@yandex.ru>
2019-01-04 20:21:28 +0300

    Create MapGenerator and his button

---
+++ Assets/Scripts/Inspector/PlatformMapGenerationEditor.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(PlatformMapGenerator))]
+public class PlatformMapGenerationEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+        
+        var script = (PlatformMapGenerator) target;
+        if(GUILayout.Button("Generate Map"))
+        {
+            script.GenerateMap();
+        }
+    }
+}
---
+++ Assets/Scripts/Platform/PlatformMapGenerator.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformMapGenerator : MonoBehaviour
+{
+    [SerializeField] private int _rowCount;
+    [SerializeField] private int _columnCount;
+    [SerializeField] private float _distanceBetweenRows;
+    [SerializeField] private float _distanceBetweenColumns;
+    [SerializeField] private GameObject _mapContainer;
+    [SerializeField] private Platform _platformPrefab;
+    
+    public void GenerateMap()
+    {
+        var currentPosition = new Vector2(_mapContainer.transform.position.x, _mapContainer.transform.position.y);
+        for (var rowInd = 0; rowInd < _rowCount; rowInd++)
+        {
+            GenerateRow(rowInd, currentPosition);
+            currentPosition += Vector2.down * _distanceBetweenRows;
+        }
+    }
+
+    private void GenerateRow(int rowInd, Vector2 currentPosition)
+    {
+        for (var columnInd = 0; columnInd < _columnCount; columnInd++)
+        {
+            var platform = Instantiate(_platformPrefab,
+                currentPosition + Vector2.right * _distanceBetweenColumns * columnInd,
+                Quaternion.identity,
+                _mapContainer.transform);
+            platform.gameObject.name = "Platform-" + (rowInd + 1) + "-" + (columnInd + 1);
+
+            if (rowInd == 0 && columnInd == 0)
+            {
+                platform.tag = TagUtils.GetTagNameByEnum(TagEnum.FirstPlatform);
+            }
+        }
+    }
+}
