29fb3d6f3b2befe1a6c59090afbebc8883c26e75
Nikita Chernyakov <nikchernyakov@yandex.ru>
2019-01-04 15:29:24 +0300

    Create Enemy generator by Key

---
+++ Assets/Scripts/Game/GenerateEnemyByKeyController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateEnemyByKeyController : MonoBehaviour
+{
+    // Update is called once per frame
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            EnemyGenerator.Instance.GenerateRandomEnemyOnRandomPosition();
+        }
+    }
+}
