9b25c0f7c9196a96ebcc5aa940ca24648734b9b3
Nikita Chernyakov <nikchernyakov@yandex.ru>
2019-01-05 10:22:15 +0300

    Create CharacterCollisionHandler

---
+++ Assets/Scripts/Character/CharacterCollisionHandler.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Liveble))]
+public class CharacterCollisionHandler : MonoBehaviour
+{
+    [SerializeField] private LayerMask _whatIsBullet;
+
+    private Liveble _liveble;
+
+    private void Awake()
+    {
+        _liveble = GetComponent<Liveble>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (GameObjectUtils.CompareLayerWithMask(other.gameObject, _whatIsBullet))
+        {
+            var bullet = other.GetComponent<Bullet>();
+            _liveble.DecreaseHp(bullet.BulletConfig.DamageCount);
+        }
+    }
+}
