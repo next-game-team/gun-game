7f1899ad216672ea2241f1eee4020c15801a3e38
Nikita Chernyakov <nikchernyakov@yandex.ru>
2019-01-05 09:46:43 +0300

    Create Dieble for Player

---
+++ Assets/Scripts/Player/PlayerDie.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDie : Dieble
+{
+    public override void Die()
+    {
+        GetComponent<Liveble>().InitHp();
+        Debug.Log("Player died. Init HP again");
+    }
+}
