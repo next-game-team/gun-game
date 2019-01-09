5d0c102a0a9e48691ee47e348f1a0c5204125adf
Nikita Chernyakov <nikchernyakov@yandex.ru>
2019-01-09 23:41:38 +0300

    Refactor Damageble
    - Rename
    - Add event on damage

---
+++ Assets/Scripts/Common/Patterns/LiveDie/Damage/Damageble.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Liveble))]
+public class Damageble : MonoBehaviour
+{
+	public delegate void DamageEvent(Damageble damageble, DamageInfo damageInfo);
+	public event DamageEvent OnDamageEvent;
+	
+	private Liveble _liveble;
+	
+	// Use this for initialization
+	private void Awake()
+	{
+		_liveble = GetComponent<Liveble>();
+	}
+
+	public void ReceiveDamage(DamageInfo damageInfo)
+	{
+		_liveble.DecreaseHp(damageInfo.DamageCount);
+		OnDamageEvent?.Invoke(this, damageInfo);
+	}
+	
+}
