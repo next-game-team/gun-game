6a2edc686b415fd904105637f503e8a1e234ea44
PsyGameStud <psygamestud@gmail.com>
2019-01-23 15:50:45 +0300

    Create Script GunRayController

---
+++ Assets/Scripts/Gun/GunRayController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class GunRayController : MonoBehaviour
+{
+    private LineRenderer _renderer;
+    void Awake()
+    {
+        _renderer = GetComponentInChildren<LineRenderer>();
+    }
+
+
+    public void TurnOn()
+    {
+        _renderer.enabled = true;
+    }
+    
+    public void TurnOff()
+    {
+        _renderer.enabled = false;
+    }
+}
