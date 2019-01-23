6e3c3ef1855c3223572efa6360af90c70689a67b
PsyGameStud <psygamestud@gmail.com>
2019-01-23 22:58:18 +0300

    Create Script CameraShakeController

---
+++ Assets/Scripts/Game/Camera/CameraShakeController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using Cinemachine;
+using System.Collections;
+
+public class CameraShakeController : MonoBehaviour
+{
+    [SerializeField, ReadOnly] private CinemachineVirtualCamera _virtualCamera;
+    private CinemachineBasicMultiChannelPerlin _virtualCameraNoise;
+
+    [SerializeField] private float _shakeAmplitude;
+    [SerializeField] private float _shakeFrequency;
+
+    void Awake()
+    {
+        _virtualCamera = GetComponent<CinemachineVirtualCamera>();
+
+        if(_virtualCamera != null)
+        {
+            _virtualCameraNoise = _virtualCamera.GetCinemachineComponent<Cinemachine.CinemachineBasicMultiChannelPerlin>();
+        }
+    }
+
+    void Start()
+    {
+        _virtualCameraNoise.m_AmplitudeGain = 0f;
+        _virtualCameraNoise.m_FrequencyGain = 0f;
+    }
+
+
+    public void ShakedCamera()
+    {
+        if(_virtualCamera != null || _virtualCameraNoise != null)
+        {
+            StartCoroutine(HarlemShake());
+        }
+    }
+
+    IEnumerator HarlemShake()
+    {
+        _virtualCameraNoise.m_AmplitudeGain = _shakeAmplitude;
+        _virtualCameraNoise.m_FrequencyGain = _shakeFrequency;
+        yield return new WaitForSeconds(0.5f);
+        _virtualCameraNoise.m_AmplitudeGain = 0f;
+        _virtualCameraNoise.m_FrequencyGain = 0f;
+    }
+}
