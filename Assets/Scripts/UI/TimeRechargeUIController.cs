bb44820f5e2ea6c8d197dfff370ed8bd123b1792
PsyGameStud <psygamestud@gmail.com>
2019-01-23 15:14:48 +0300

    Update TimeRechargeUIController.cs

--- Assets/Scripts/UI/TimeRechargeUIController.cs
+++ Assets/Scripts/UI/TimeRechargeUIController.cs
@@ -1,18 +1,22 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class TimeRechargeUIController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Image _img;
+    private GameObject _player;
+    private PlayerAttackWithTimeManager _timeManager;
+
+
+    void Awake()
     {
-        
+        _player = GameObject.FindGameObjectWithTag("Player");
+        _timeManager = _player.GetComponent<PlayerAttackWithTimeManager>();
+        _img = GetComponent<Image>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        _img.fillAmount = (_timeManager.CurrentTimeEnergy / _timeManager.TimeEnergyCapacity);
     }
 }
