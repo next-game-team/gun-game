bdce4394e1622e8f0d1af6c78ad1f19965a1bfaa
Nikita Chernyakov <nikchernyakov@yandex.ru>
2019-02-07 00:19:13 +0300

    Update PlayerAttackController.cs
    Fix checking key and touch controll

--- Assets/Scripts/Player/PlayerAttackController.cs
+++ Assets/Scripts/Player/PlayerAttackController.cs
@@ -5,6 +5,8 @@ public class PlayerAttackController : AttackController
     public delegate void AttackStartEvent();
     public event AttackStartEvent OnAttackStartEvent;
 
+    private bool _attackByTouch = false;
+
     protected override void CheckInput()
     {
         if (Input.GetKeyDown(KeyCode.Space) ||
@@ -12,11 +14,13 @@ public class PlayerAttackController : AttackController
             Input.touchCount == (TouchManager.Instance.IsInDrag ? 2 : 1))
         {
             OnAttackStartEvent?.Invoke();
+            _attackByTouch = Input.touchCount == (TouchManager.Instance.IsInDrag ? 2 : 1);
         }
         else if (Input.GetKeyUp(KeyCode.Space) ||
-                 Input.touchCount == (TouchManager.Instance.IsInDrag ? 1 : 0))
+                 (Input.touchCount == (TouchManager.Instance.IsInDrag ? 1 : 0) && _attackByTouch))
         {
             AttackEvent?.Invoke();
+            _attackByTouch = false;
         }
     }
 }
