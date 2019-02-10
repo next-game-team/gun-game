397d44db89df62cdbc6c7ab9f963bffbe19ab271
PsyGameStud <psygamestud@gmail.com>
2019-02-10 20:56:45 +0300

    Create script TextWaveController

---
+++ Assets/Scripts/UI/TextWaveController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class TextWaveController : MonoBehaviour
+{
+    private TextMeshProUGUI _text;
+
+    public int CurrentWave {get; private set;}
+    public int TotalWaves {get; private set;}
+
+    void Awake()
+    {
+        _text = GetComponent<TextMeshProUGUI>();
+    }
+
+    public void SetWaveNumText(int _curWave, int _totWave)
+    {
+        CurrentWave = _curWave;
+        TotalWaves = _totWave;
+
+        _text.text = "Wave " + CurrentWave.ToString() + " / " + TotalWaves.ToString();
+    }
+   
+}
