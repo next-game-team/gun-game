22c83271765f3e284b5f48258a5dc487647d742b
Nikita Chernyakov <nikchernyakov@yandex.ru>
2019-02-13 22:36:18 +0300

    Create ControlTutorial

--- Assets/Scripts/UI/CanvasController.cs
+++ Assets/Scripts/UI/CanvasController.cs
@@ -4,7 +4,9 @@ public class CanvasController : MonoBehaviour
 {
     [SerializeField] private GameObject _pausePanel;
     [SerializeField] private GameObject _deathScreen;
+    [SerializeField] private ControlTutorial _controlTutorial;
 
     public GameObject PausePanel => _pausePanel;
     public GameObject DeathScreen => _deathScreen;
+    public ControlTutorial ControlTutorial => _controlTutorial;
 }
